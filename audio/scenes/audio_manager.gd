122de4d7e3274086b68bf53141a6c589b6a5a1a4
Reverron <131873663+Reverron@users.noreply.github.com>
2026-02-05 12:37:32 +0100

    Audio Manager Optimization

--- audio/scenes/audio_manager.gd
+++ audio/scenes/audio_manager.gd
@@ -20,6 +20,7 @@ var music_bus: FmodBus
 var sfx_bus: FmodBus
 
 ## -- Playback Settings --
+var emitter_list: Array
 const PLAYING = FmodServer.FMOD_STUDIO_PLAYBACK_PLAYING
 const STOPPED = FmodServer.FMOD_STUDIO_PLAYBACK_STOPPED
 
@@ -30,11 +31,18 @@ const MUFFLE: String = "Muffle"
 const TRUE: String = "true"
 const FALSE: String = "false"
 
-func _ready() -> void: load_banks()
+func _ready() -> void:
+	load_banks()
+	load_emitters()
 
 func load_banks() -> void:
 	bank_list["master_strings_bank"] = FmodServer.load_bank(MASTER_STRINGS_BANK, FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
 	bank_list["master_bank"] = FmodServer.load_bank(MASTER_BANK, FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
+
+func load_emitters() -> void:
+	for emitter in get_children():
+		if emitter is FmodEventEmitter2D:
+			emitter_list.append(emitter)
 
 func play(
 sound_path: String, 
@@ -51,23 +59,31 @@ value: Variant = null
 	if value is float: instance.set_parameter_by_name(parameter, value)
 	if value is String: instance.set_parameter_by_name_with_label(parameter, value, false)
 	else: pass
-	
+
 	instance.start()
 	instance.release()
 
 func play_instance(sound_path: String, object_transform: Transform2D) -> FmodEvent:
 	if sound_path == null: push_error("audio missing")
-	
+
 	var instance: FmodEvent = FmodServer.create_event_instance(sound_path)
 	instance.set_3d_attributes(object_transform)
 	instance.start()
 	return instance
 
-func clear_instance(instance: FmodEvent) -> void:
-	if instance == null: return
-	
-	instance.stop(FmodServer.FMOD_STUDIO_STOP_ALLOWFADEOUT)
-	instance.release()
+func clear_instance(instances: Array[FmodEvent]) -> void:
+	if instances == null: return
+
+	for instance in instances:
+		instance.stop(FmodServer.FMOD_STUDIO_STOP_ALLOWFADEOUT)
+		instance.release()
+
+func clear_emitter(instances: Array[FmodEventEmitter2D]) -> void:
+	if instances == null: return
+
+	for emitter in instances:
+		emitter.stop()
+		emitter.release()
 
 func muffle(force: bool = false, state: bool = false) -> void: 
 	if force and state:
@@ -83,3 +99,5 @@ func muffle(force: bool = false, state: bool = false) -> void:
 	else:
 		FmodServer.set_global_parameter_by_name_with_label(MUFFLE, FALSE)
 		muffled = false
+
+func free() -> void: clear_emitter(emitter_list)
