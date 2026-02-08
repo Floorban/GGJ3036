db32801f065e0a8c26c88614e7bc75b51a36b2e0
Eric Li <15696082812@163.com>
2026-02-08 21:12:07 +0100

    audio missing

--- game/level/combat_area/combat_area.gd
+++ game/level/combat_area/combat_area.gd
@@ -188,7 +188,7 @@ func end_battle() -> void:
 	)
 	
 	var current_val = retro_mat.get_shader_parameter("color_quant_steps")
-	tween.tween_property(retro_mat, "shader_parameter/color_quant_steps", 12.0, 0.3).from(current_val)
+	tween.tween_property(retro_mat, "shader_parameter/color_quant_steps", 10.0, 0.3).from(current_val)
 	
 	tween.tween_property(
 			camera,
