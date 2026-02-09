a33e20b91168369be2e2525238d27203e6c41230
Eric Li <15696082812@163.com>
2026-02-09 01:07:55 +0100

    eric delay

---
+++ game/system/character/enemy/boss/eric.gd
@@ -0,0 +1,20 @@
+extends Enemy
+
+var eric_start := false
+
+func start_round() -> void:
+	await get_tree().create_timer(10.0).timeout
+	super.start_round()
+
+func init_character() -> void:
+	_init_anatomy_parts()
+	_init_combat_component()
+	get_anatomy_references()
+	await get_tree().create_timer(1.0).timeout
+	DialogueManager.say("This multi eyed beast.. he will take a bit of time to prepare his attack")
+	await DialogueManager.wait_for_dialogue_continue()
+	DialogueManager.say("but dont give him the chance, once he starts attacking")
+	await DialogueManager.wait_for_dialogue_continue()
+	DialogueManager.say("HE WON'T STOP")
+	await get_tree().create_timer(3.0).timeout
+	enemy_dialogue_end.emit()
