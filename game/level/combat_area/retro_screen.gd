bdc4ace845de395119c962776b2f42eb2a918835
Reverron <131873663+Reverron@users.noreply.github.com>
2026-02-18 16:43:50 +0100

    fix attempt 1

--- game/level/combat_area/retro_screen.gd
+++
@@ -1,9 +0,0 @@
-class_name RetroScreen extends ColorRect
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func trans_to_break() -> void:
-	animation_player.play("TransitionToRest")
-
-func trans_to_combat() -> void:
-	animation_player.play_backwards("TransitionToRest")
