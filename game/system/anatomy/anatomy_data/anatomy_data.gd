a560569974f3f7cd7b5328b3e85e8927eb821bfa
Eric Li <15696082812@163.com>
2026-02-20 14:28:49 +0100

    ff

--- game/system/anatomy/anatomy_data/anatomy_data.gd
+++ game/system/anatomy/anatomy_data/anatomy_data.gd
@@ -9,11 +9,11 @@ func get_anatomy_type() -> Anatomy.AnatomyType
 
 @export var anatomy_stats := {
 	Stats.StatType.MAX_HP: 0.0,
-	Stats.StatType.COOLDOWN: -0.1,
-	Stats.StatType.DAMAGE: 0.1,
-	Stats.StatType.ATTACK_SPEED: 0.1,
+	Stats.StatType.COOLDOWN: 0.0,
+	Stats.StatType.DAMAGE: 0.0,
+	Stats.StatType.ATTACK_SPEED: 0.0,
 	Stats.StatType.CRIT_CHANCE: 0.0,
 	Stats.StatType.CRIT_DAMAGE: 0.0,
-	Stats.StatType.STUN_STRENGTH: 1.0,
-	Stats.StatType.STUN_RESIST: 1.0
+	Stats.StatType.STUN_STRENGTH: 0.0,
+	Stats.StatType.STUN_RESIST: 0.0
 }
